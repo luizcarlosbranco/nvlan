3e876b39d4c827793540fa6ff3360ebb5cc1a30f
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2022-04-28 16:50:09 -0300

    SQL query with Powershell
    A simple example

---
+++ SQL_query_with_Powershell/SQL_query_with_Powershell.ps1
@@ -0,0 +1,20 @@
+﻿#----------------------------------------------------------------------------------------------------------------------
+# Change THIS variables below
+$DB_Server = "SERVERNAME.yourdomain.com"
+$DB_Database = "DATABASE_NAME"
+$DB_Username = "DATABASE_USERNAME"
+$DB_Password = "DATABASE_PASSWORD"
+$DB_Command = "select * from TABLE"
+#----------------------------------------------------------------------------------------------------------------------
+# Running
+Write-Host -NoNewline "Conectando ao Banco... "
+$connection = New-Object System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = "Server=$DB_Server;uid=$DB_Username; pwd=$DB_Password;Database=$DB_Database;Integrated Security=False;"
+$connection.Open()
+ 	$command = $connection.CreateCommand()
+    $command.CommandText = $DB_Command
+   	$result = $command.ExecuteReader()
+    $table = new-object "System.Data.DataTable"
+    $table.Load($result)
+$connection.Close()
+$array = @($table)
