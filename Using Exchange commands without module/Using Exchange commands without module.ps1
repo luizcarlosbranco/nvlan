6bbf188bd56ebf3e9e5a59af259d54d70ed0103b
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2022-04-29 16:49:45 -0300

    Using Exchange Server ConnectionUri

---
+++ Using Exchange commands without module/Using Exchange commands without module.ps1
@@ -0,0 +1,5 @@
+﻿$QUALRELAY = "YOUR_EXCHANGE_SERVER.yourdomain.com"
+$psCred = Get-Credential -Message "Informe credencial ADMIN"
+Import-PSSession (New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$QUALRELAY/PowerShell/ -Credential $psCred -Authentication Kerberos)
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+Clear-Host
