ec5429775e70cc60766b8bb3f71ea9c9f6a9ca0b
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2022-09-05 11:35:04 -0300

    Update Import or export 365Photos.ps1

--- Exchange/Import or export 365Photos.ps1
+++ Exchange/Import or export 365Photos.ps1
@@ -2,7 +2,16 @@
 While ( $continue -eq $false ) {
     Clear-host
     $email = Read-Host "Insert the email (like user@yourdomain.com)"
+    If ( ($email -notcontains "@") ){
+        $domain = $env:USERDNSDOMAIN
+        $domain = $domain.tolower()
+        $email = $email +"@"+ $domain
+    }
     $folder = Read-Host "Insert the folder (like C:\Temp)"
+    If ( ($folder -eq "") -or !(Test-Path -Path $folder) ){
+        write-host "Using C:\Temp"
+        $folder = "C:\Temp"
+    }
     $choose = Read-Host "Type import (to import), or export (to export)"
     If ( ($email -like "*@*") -and ($email -like "*.*") -and (Test-Path -Path "$folder") -and ( ($choose = "import") -OR ($choose = "export") ) ){
         $continue = $True
