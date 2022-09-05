4168ecfe0e94b6af33a6fa6d507d60fbbcabdaad
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2022-09-05 11:36:57 -0300

    Add files via upload

---
+++ General in O365/Import or export 365Photos.ps1
@@ -0,0 +1,23 @@
+﻿$continue = $false
+While ( $continue -eq $false ) {
+    Clear-host
+    $email = Read-Host "Insert the email (like user@yourdomain.com)"
+    $folder = Read-Host "Insert the folder (like C:\Temp)"
+    $choose = Read-Host "Type import (to import), or export (to export)"
+    If ( ($email -like "*@*") -and ($email -like "*.*") -and (Test-Path -Path "$folder") -and ( ($choose = "import") -OR ($choose = "export") ) ){
+        $continue = $True
+    }
+    Else
+    {
+        Write-Host "Try againg..."
+        Read-Host
+    }
+}
+#-------------------------------------------------------------------------
+Connect-ExchangeOnline
+$file = $email.Split("@")[0]+".jpg"
+switch ( $choose )
+{
+    import { Set-UserPhoto $email -PictureData ([System.IO.File]::ReadAllBytes("$folder\$file")) }
+    export { (Get-UserPhoto $email).PictureData | Set-Content "$folder\$file" -Encoding byte }
+}
