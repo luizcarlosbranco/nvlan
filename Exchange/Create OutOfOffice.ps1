ec8a112ebc7cb6bdcd8dac384566f72f92edc33d
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2022-09-05 11:32:54 -0300

    Add files via upload

---
+++ Exchange/Create OutOfOffice.ps1
@@ -0,0 +1,12 @@
+﻿#1 - RUN AS ADMIN
+#If it is the first time, run the command: Install-Module -Name ExchangeOnlineManagement -RequiredVersion 1.0.1
+#----------------------------------------------------------------------------------------------------------------------------------------
+$message = "OOO Message.<br><br>Thist is a OutOfOffice Mesage<br><br>Contact the support team."
+$mailbox = "the_email@yourdomain.com.br"
+#----------------------------------------------------------------------------------------------------------------------------------------
+#enabling modules
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline
+#----------------------------------------------------------------------------------------------------------------------------------------
+Set-MailboxAutoReplyConfiguration -Identity $mailbox -AutoReplyState Scheduled -StartTime "12/27/2021 00:00:01" -EndTime "1/9/2022 23:59:59" -InternalMessage $message -ExternalMessage $message
+#Set-MailboxAutoReplyConfiguration -Identity $mailbox -AutoReplyState Enabled -InternalMessage $message -ExternalMessage $message
