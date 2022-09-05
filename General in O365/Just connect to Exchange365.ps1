c875da2439050e11ac93e29a0a5671b8951b3ddc
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2022-09-05 11:41:17 -0300

    Add files via upload

---
+++ General in O365/Just connect to Exchange365.ps1
@@ -0,0 +1,10 @@
+﻿#Run AS ADMIN
+#In the first time, run:
+Install-Module -Name ExchangeOnlineManagement -RequiredVersion 1.0.1
+
+#With the module, execute:
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -ShowProgress $true
+
+#Now, just test:
+Get-AddressList
