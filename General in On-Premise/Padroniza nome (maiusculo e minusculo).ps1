8520ef129fe7ab9d3d4c1a2bf8147b429edc8ee6
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2023-06-20 19:11:20 -0300

    Add files via upload

---
+++ General in On-Premise/Padroniza nome (maiusculo e minusculo).ps1
@@ -0,0 +1,24 @@
+﻿$NOME_COMPLETO = Read-Host -Prompt 'Informe o nome completo da pessoa (ex: Fulanos da Silva Sauro)'
+$NameRAW = $NOME_COMPLETO.ToLower().Replace("."," ").Replace("-"," ").Replace(","," ").Replace("  "," ").Trim(" ")
+$NameRAW = [regex]::Replace($NameRAW,"\s+"," ").Trim()
+$Array_Nome = $NameRAW.Split(" ")
+$NOME_COMPLETO = $null
+$espaco = $null
+$name = $null
+$ligacao = 'e','de','da','das','do','dos'
+$i=0
+while($i -lt $Array_Nome.Count)
+{
+    $name = $Array_Nome[$i]
+    if ($ligacao.Contains($name))
+    {
+        $NOME_COMPLETO = $NOME_COMPLETO+" "+$name
+    }
+    else
+    {
+        $NOME_COMPLETO = $NOME_COMPLETO+""+$espaco+""+$name.substring(0,1).toupper()+$name.substring(1).tolower()
+    }
+    $espaco = " "
+    $i = $i+1
+}
+echo "$NOME_COMPLETO"
