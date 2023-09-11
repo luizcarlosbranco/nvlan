1ef8b96b4a4f10c85a783c27d8cf9ac2521b4c0c
luizcarlosbranco <104591263+luizcarlosbranco@users.noreply.github.com>
2023-09-11 16:52:53 -0300

    Add files via upload

---
+++ General in On-Premise/Read Logs of RDP-Logon.ps1
@@ -0,0 +1,14 @@
+﻿cls
+Get-WinEvent -Logname Security -FilterXPath "*[System[EventID=4624]]" | ForEach-Object {
+    $data = $_.TimeCreated.tostring().replace("2020 ","2020;")
+    $message = $_.Message -split "`n"
+    $message | Select-String -Pattern '	Account Name:	' | ForEach-Object { $username=$_.tostring().replace("	Account Name:	","").Trim(" ").Trim("	")}
+    if (($data -ne $data1) -and ($username -ne $username1))
+    {
+        echo "$data;$username"
+    }
+    $data1 = $data
+    $username1 = $username
+}
+echo "--------------------------"
+echo "Finalizado"
